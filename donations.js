5f2600c9d7f8e55bf73cb3270e093ebe1b5c960f
Travis Rollins <troll59910@aol.com>
2019-07-26 10:40:07 -0600

    Simplify data on api

--- donations.js
+++ donations.js
@@ -4,13 +4,11 @@ module.exports = {
       id: 1,
       name: 'Travis Rollins',
       donation: 200,
-      animal_id: 2
     },
     {
       id: 2,
       name: 'Leta Keane',
       donation: 400,
-      animal_id: 1
     }
   ]
 }
